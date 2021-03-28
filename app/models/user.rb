9887318811657f71e8b503c3f95d144e346e615e
abeidahmed <abeidahmed92@gmail.com>
2021-03-28 13:46:14 +0530

    Add user model validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,22 @@
 class User < ApplicationRecord
+  has_secure_password
+  has_secure_token :auth_token
+
+  before_validation :strip_email_address
+  before_save :lowercase_email_address
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+
+  validates :email_address, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false } # rubocop:disable Layout/LineLength
+  validates :password, presence: true, length: { minimum: 6 }
+
+  private
+
+  def strip_email_address
+    self.email_address = email_address.to_s.strip
+  end
+
+  def lowercase_email_address
+    self.email_address = email_address.to_s.downcase
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,42 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject(:user) { build(:user) }
+
+  describe "validations" do
+    it { is_expected.to have_secure_password }
+
+    it { is_expected.to validate_presence_of(:email_address) }
+
+    it { is_expected.to validate_uniqueness_of(:email_address).case_insensitive }
+
+    it { is_expected.to validate_length_of(:email_address).is_at_most(255) }
+
+    it { is_expected.to allow_value("johndoe@example.com", "johjn@exa.co.in").for(:email_address) }
+
+    it { is_expected.not_to allow_value("johndoeexample.com", "johjn@exa").for(:email_address) }
+
+    it "lowercases email_address before saving" do
+      email_address = "hello@example.com"
+      user.email_address = email_address.upcase
+      user.save!
+
+      expect(user.email_address).to eq(email_address)
+    end
+
+    it "strips the email_address before validation" do
+      email_address = "   hello@example.com "
+      user.email_address = email_address
+      user.save!
+
+      expect(user.email_address).to eq("hello@example.com")
+    end
+
+    it "generates user auth_token at random" do
+      user.auth_token = nil
+      user.save!
+
+      expect(user.auth_token).to be_present
+    end
+  end
 end

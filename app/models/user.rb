ed1e43e3949b27eec99df569cc74218986051f3f
abeidahmed <abeidahmed92@gmail.com>
2021-03-28 14:45:10 +0530

    Setup user, web_link model association

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_many :web_links, dependent: :destroy
+
   has_secure_password
   has_secure_token :auth_token
 
--- spec/models/web_link_spec.rb
+++ spec/models/web_link_spec.rb
@@ -1,5 +1,7 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe WebLink, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "associations" do
+    it { is_expected.to belong_to(:user) }
+  end
 end

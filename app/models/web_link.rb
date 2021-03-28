609717a1eee98b74e15b5036f9bff0b4b58ca433
abeidahmed <abeidahmed92@gmail.com>
2021-03-28 15:08:04 +0530

    Add web_link model validations

--- app/models/web_link.rb
+++ app/models/web_link.rb
@@ -1,3 +1,18 @@
 class WebLink < ApplicationRecord
   belongs_to :user
+
+  after_create_commit { generate_token(:uid) }
+
+  VALID_URL_REGEX = /\A(http|https):\/\/[a-z0-9]+([\-.]{1}[a-z0-9]+)*\.[a-z]{2,5}(([0-9]{1,5})?\/.*)?$\z/ix
+
+  validates :original_url, format: { with: VALID_URL_REGEX }
+
+  private
+
+  def generate_token(column)
+    loop do
+      self[column] = Digest::SHA256.hexdigest(original_url)[0..6]
+      break unless self.class.exists?(column => self[column])
+    end
+  end
 end
--- spec/models/web_link_spec.rb
+++ spec/models/web_link_spec.rb
@@ -1,7 +1,28 @@
 require "rails_helper"
 
 RSpec.describe WebLink, type: :model do
+  subject(:web_link) { build(:web_link) }
+
   describe "associations" do
     it { is_expected.to belong_to(:user) }
+  end
+
+  describe "validations" do
+    it do
+      expect(web_link).to allow_value(
+        "https://goog.com", "https://www.hello.com", "http://gool.com", "http://www.googl.co.in"
+      ).for(:original_url)
+    end
+
+    it { is_expected.not_to allow_value("https://goog", "www.hello.com").for(:original_url) }
+  end
+
+  describe "callbacks" do
+    it "generates a random uid based on the original_url" do
+      web_link.uid = ""
+      web_link.save!
+
+      expect(web_link.uid).to be_present
+    end
   end
 end

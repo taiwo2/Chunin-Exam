1ad5a5fdf60cb73e80eee7a654de6c53d7903d14
abeidahmed <abeidahmed92@gmail.com>
2021-03-28 19:59:54 +0530

    Raise error if web_link does not exist

--- app/controllers/web_links_controller.rb
+++ app/controllers/web_links_controller.rb
@@ -1,7 +1,6 @@
 class WebLinksController < ApplicationController
   def show
-    web_link = WebLink.find_by(uid: params[:id])
-    return unless web_link
+    web_link = WebLink.find_by!(uid: params[:id])
 
     redirect_to web_link.original_url
   end

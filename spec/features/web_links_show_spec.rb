86e85a7399152d6f28ee8aa754ce108a1da91ef5
abeidahmed <abeidahmed92@gmail.com>
2021-03-28 19:57:35 +0530

    Add web link shortened url test

--- spec/features/web_links_show_spec.rb
+++ spec/features/web_links_show_spec.rb
@@ -9,4 +9,15 @@ RSpec.describe "WebLinksShows", type: :feature do
 
     expect(page).to have_text(web_link.title)
   end
+
+  it "displays the web link shortened url" do
+    user = create(:user)
+    web_link = create(:web_link, user: user)
+    feature_sign_in(user)
+    visit app_web_link_path(web_link)
+
+    within "#web-url-action" do
+      expect(page).to have_link(external_web_link_url(id: web_link.uid))
+    end
+  end
 end

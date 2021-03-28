7cdb8013f93c79fdfbff3e1a94fb9524d995a453
abeidahmed <abeidahmed92@gmail.com>
2021-03-28 21:39:46 +0530

    Add url shortener form and relevant tests

---
+++ spec/features/app/shorten_url_spec.rb
@@ -0,0 +1,15 @@
+require "rails_helper"
+
+RSpec.describe "App::ShortenUrls", type: :feature do
+  it "redirects to web_link show page after shortening the url" do
+    user = create(:user)
+    feature_sign_in(user)
+    visit app_web_links_path
+    within "#new_web_link", visible: false do
+      fill_in "Website URL", with: "https://google.com", visible: false
+      click_button "Shorten", visible: false
+    end
+
+    expect(page).to have_current_path(app_web_link_path(WebLink.first))
+  end
+end

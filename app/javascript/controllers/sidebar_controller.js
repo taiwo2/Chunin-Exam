8ed458859b6e97814cd75fad4883e6ab177f58ed
abeidahmed <abeidahmed92@gmail.com>
2021-03-29 11:41:48 +0530

    Fix: sidebar re-render on turbolinks load

--- app/javascript/controllers/sidebar_controller.js
+++ app/javascript/controllers/sidebar_controller.js
@@ -3,12 +3,6 @@ import ApplicationController from "./application_controller";
 export default class extends ApplicationController {
   static targets = ["aside"];
 
-  connect() {
-    if (this.pageIsPreview) {
-      this.asideTarget.setAttribute("hidden", "");
-    }
-  }
-
   show() {
     this.asideTarget.classList.add("active");
   }

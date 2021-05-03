d520acae36e697c8411145fee445cbc45e61b9dd
taiwo2 <adetonat@ymail.com>
2021-05-03 18:41:45 +0100

    Configure route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-  get 'shorts/show'
+  get 'shorts/index'
+  root 'static_pages#home'
+  resources :shorts, only: [:show, :create, :new]
+  resources :advertisements
+  get ":id", to: "static_pages#redirect"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

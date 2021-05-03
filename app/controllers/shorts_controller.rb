caf9d6e4cbe74e9b55901dd60a2a1449d0e8b6af
taiwo2 <adetonat@ymail.com>
2021-05-03 18:29:34 +0100

    Add methos to short controller

--- app/controllers/shorts_controller.rb
+++ app/controllers/shorts_controller.rb
@@ -1,4 +1,36 @@
 class ShortsController < ApplicationController
+ before_action :set_short, only: [:show]
+
+ 
   def show
   end
+
+  def new
+    @short = Short.new
+  end
+
+  def create
+    @short = Short.new(short_params)
+    respond_to do |format|
+      if @short.save
+        format.html { redirect_to root_path, notice: 'Short was successfully created.' }
+        format.json { render :new, status: :created, location: @short }
+      else
+        format.html { render :new }
+        format.json { render json: @short.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+
+  private
+  
+    def set_short
+      @short = Short.find(params[:id])
+    end
+
+    def short_params
+      params.require(:short).permit(:url)
+    end
+
 end

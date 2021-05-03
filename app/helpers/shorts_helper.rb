e00ea16712c3c4e1622650c59583f936f114b57b
taiwo2 <adetonat@ymail.com>
2021-05-03 18:52:09 +0100

    Add  style for views

--- app/helpers/shorts_helper.rb
+++ app/helpers/shorts_helper.rb
@@ -1,9 +1,9 @@
 module ShortsHelper
   def link_url(url)
-      link_to url, url, :target => "_blank" 
+    link_to url, url, :target => "_blank" 
   end
-  
+
   def short(id)
-      Short.find(id)
+    Short.find(id)
   end
 end

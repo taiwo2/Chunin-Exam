43c6f8064e47fa6dc9cbae5a9723dd58ab1af214
taiwo2 <adetonat@ymail.com>
2021-05-03 21:41:39 +0100

    Add static_pages home.html logic

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   get 'shorts/index'
   root 'static_pages#home'
   resources :shorts, only: [:show, :create, :new]
-  resources :advertisements
   get ":id", to: "static_pages#redirect"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

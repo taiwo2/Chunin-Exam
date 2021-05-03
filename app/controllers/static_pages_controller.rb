e2ee712543906d5a33b01b990736bb9c59ebc65a
taiwo2 <adetonat@ymail.com>
2021-05-03 18:33:01 +0100

    Add shorts_helper

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,10 @@
 class StaticPagesController < ApplicationController
   def home
+    @short = Short.new
+    @short2 = Short.last
+  end
+
+  def redirect
+    @redirection = Short.find(params[:id].to_i(36)).url
   end
 end

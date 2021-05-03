639a10c0c7beff9e4f76c0f9f76bd9d1e40d9d3e
taiwo2 <adetonat@ymail.com>
2021-05-03 22:39:27 +0100

    Add static _pages__controller_rspec.rb

--- spec/controllers/shorts_controller_spec.rb
+++ spec/controllers/shorts_controller_spec.rb
@@ -3,11 +3,11 @@ require 'rails_helper'
 RSpec.describe ShortsController, type: :controller do
 
   let(:valid_attributes) {
-    {id: 1, url: 'http://algo.com'}
+    { id: 1, url: 'http://algo.com' }
   }
 
   let(:invalid_attributes) {
-    {id:1 , url: ''}
+    { id: 1, url: '' }
   }
 
   let(:valid_session) { {} }
@@ -31,33 +31,32 @@ RSpec.describe ShortsController, type: :controller do
     context 'with valid params' do
       it 'creates a new Short' do
         expect {
-          post :create, params: {short: valid_attributes}, session: valid_session
+          post :create, params: { short: valid_attributes }, session: valid_session
         }.to change(Short, :count).by(1)
       end
 
       it 'assigns a newly created short as @short' do
-        post :create, params: {short: valid_attributes}, session: valid_session
+        post :create, params: { short: valid_attributes }, session: valid_session
         expect(assigns(:short)).to be_a(Short)
         expect(assigns(:short)).to be_persisted
       end
 
-      it 'redirects to the home page'do
-        post :create, params: {short: valid_attributes}, session: valid_session
+      it 'redirects to the home page' do
+        post :create, params: { short: valid_attributes }, session: valid_session
         expect(response).to redirect_to(root_path)
       end
     end
 
     context 'with invalid params' do
       it 'assigns a newly created but unsaved short as @short' do
-        post :create, params: {short: invalid_attributes}, session: valid_session
+        post :create, params: { short: invalid_attributes }, session: valid_session
         expect(assigns(:short)).to be_a_new(Short)
       end
 
-      it 're-renders the 'new' template' do
-        post :create, params: {short: invalid_attributes}, session: valid_session
+      it 're-renders the new template' do
+        post :create, params: { short: invalid_attributes }, session: valid_session
         expect(response).to render_template('new')
       end
     end
   end
-
 end

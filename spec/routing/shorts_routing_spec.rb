d289fc63a624c808a3389e5e8035ebf2af7aa630
taiwo2 <adetonat@ymail.com>
2021-05-04 23:01:58 +0100

    Update rspec

--- spec/routing/shorts_routing_spec.rb
+++ spec/routing/shorts_routing_spec.rb
@@ -1,31 +1,22 @@
 require "rails_helper"
 
 RSpec.describe ShortsController, type: :routing do
-  describe "routing" do
+  describe 'routing' do
 
-    it "routes to #index" do
-      expect(:get => "/admin/shorts").to route_to("admin/shorts#index")
+    it 'routes to #index' do
+      expect(get: '/shorts').to route_to('shorts#index')
     end
 
-    it "routes to #new" do
-      expect(:get => "/shorts/new").to route_to("shorts#new")
+    it 'routes to #new' do
+      expect(get: '/shorts/new').to route_to('shorts#new')
     end
 
-    it "routes to #show" do
-      expect(:get => "/shorts/1").to route_to("shorts#show", :id => "1")
-    end
-    
-    it "routes to admin#show" do
-      expect(:get => "admin/shorts/1").to route_to("admin/shorts#show", :id => "1")
+    it 'routes to #show' do
+      expect(get: '/shorts/1').to route_to('shorts#show', id: '1')
     end
 
-    it "routes to #create" do
-      expect(:post => "/shorts").to route_to("shorts#create")
+    it 'routes to #create' do
+      expect(post: '/shorts').to route_to('shorts#create')
     end
-
-    it "routes to #destroy" do
-      expect(:delete => "/admin/shorts/1").to route_to("admin/shorts#destroy", :id => "1")
-    end
-
   end
 end
--- spec/routing/users_routing_spec.rb
+++ spec/routing/users_routing_spec.rb
@@ -1,31 +1,30 @@
 require "rails_helper"
 
 RSpec.describe Admin::UserController, type: :routing do
-  describe "routing" do
+  describe 'routing' do
 
-    it "routes to #index" do
-      expect(:get => "/admin/user").to route_to("admin/user#index")
-    end
-
-    it "routes to #show" do
-      expect(:get => "/admin/user/1").to route_to("admin/user#show", :id => "1")
+    it 'routes to #index' do
+      expect(get: '/user').to route_to('user#index')
     end
 
-    it "routes to #destroy" do
-      expect(:delete => "/admin/user/1").to route_to("admin/user#destroy", :id => "1")
-    end
-    
-    it "routes to #edit" do
-      expect(:get => "/admin/user/1/edit").to route_to("admin/user#edit", :id => "1")
-    end
-    
-    it "routes to #update via PUT" do
-      expect(:put => "/admin/user/1").to route_to("admin/user#update", :id => "1")
+    it 'routes to #show' do
+      expect(get: '/user/1').to route_to('user#show', id: '1')
     end
 
-    it "routes to #update via PATCH" do
-      expect(:patch => "/admin/user/1").to route_to("admin/user#update", :id => "1")
+    it 'routes to #destroy' do
+      expect(delete: '/user/1').to route_to('user#destroy', id: '1')
     end
 
+    it 'routes to #edit' do
+      expect(get: '/user/1/edit').to route_to('user#edit', id: '1')
+    end
+
+    it 'routes to #update via PUT' do
+      expect(put: '/user/1').to route_to('admin/user#update', id: '1')
+    end
+
+    it 'routes to #update via PATCH' do
+      expect(patch: '/user/1').to route_to('user#update', id: '1')
+    end
   end
 end
